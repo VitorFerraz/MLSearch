57466269b708e9da34e92779145c12005e2a25c1
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2021-02-10 17:21:12 -0300

    Initial Commit

---
+++ MLSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLSearch
+//
+//  Created by Vitor Ferraz Varela on 10/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
